87c0be7337624dc480316ac549e2630c6a43496e
FlashBlaze <neerajlagwankar@gmail.com>
2017-04-02 16:53:08 +0530

    Third commit

--- examples.py
+++ examples.py
@@ -39,3 +39,34 @@ print array %(1, 2, 3)
 print array %('one', 'two', 'three')
 print array %(array, array, array) #if you give only 2 arguments, it will not work
 print array %("You can even", "write", "sentences")
+
+print "*" * 39
+
+print """ you can multiple lines
+with
+three double quotes
+this is just awesome! """ #do not use spaces between double quotes
+
+print "\a" #bell sound
+
+# \\ 	Backslash (\)
+# \' 	Single-quote (')
+# \" 	Double-quote (")
+# \a 	ASCII bell (BEL)
+# \b 	ASCII backspace (BS)
+# \f 	ASCII formfeed (FF)
+# \n 	ASCII linefeed (LF)
+# \N{name} 	Character named name in the Unicode database (Unicode only)
+# \r 	Carriage Return (CR)
+# \t 	Horizontal Tab (TAB)
+# \uxxxx 	Character with 16-bit hex value xxxx (u'' string only)
+# \Uxxxxxxxx 	Character with 32-bit hex value xxxxxxxx (u'' string only)
+# \v 	ASCII vertical tab (VT)
+# \ooo 	Character with octal value ooo
+# \xhh 	Character with hex value hh
+
+#while True:
+#	for i in ["/","-","|","\\","|"]:
+#       	print "%s\r" % i,             cool program to try!
+
+
