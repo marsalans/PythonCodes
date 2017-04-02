3ae92681cf73041ff10bbba5148c0f6474b34656
FlashBlaze <neerajlagwankar@gmail.com>
2017-04-02 14:04:10 +0530

    Importing modules and working with them

---
+++ examples3.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+# run command like this: python ex13.py first 2nd 3rd in cmd
+# first, 2nd 3rd names can be any names
+
