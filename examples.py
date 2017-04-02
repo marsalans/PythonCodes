406a367ef166520ae526d4dcff7dbfc072e6a10e
FlashBlaze <neerajlagwankar@gmail.com>
2017-04-02 11:22:17 +0530

    Second commit

--- examples.py
+++ examples.py
@@ -3,3 +3,9 @@ milk = 2
 print "I am buying %d" % butter, "g butter and %d" % milk, "l milk"
 shirt_colour = 'red'
 print "I am wearing %r" % shirt_colour, "colour shirt" #by using format specifier 'r' you tell python to print it no matter what. 's' is used for printing string
+													   #notice that i have not given comma after the 1st print message as i am using format specifier
+is_python = 'cool'
+print "learning python is %s" % is_python #see that i have not used double quotes for cool, stil it is being printed
+grade1 = 'A'
+grade2 = "B"
+print "I have got %s in Math and %s in Geography" % (grade1, grade2) 
