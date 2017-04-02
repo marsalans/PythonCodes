d219bd0832cd87546adf30d98a64eff400f5257a
FlashBlaze <neerajlagwankar@gmail.com>
2017-04-02 10:56:12 +0530

    First commit

---
+++ examples.py
@@ -0,0 +1,5 @@
+butter = 100
+milk = 2
+print "I am buying %d" % butter, "g butter and %d" % milk, "l milk"
+shirt_colour = 'red'
+print "I am wearing %r" % shirt_colour, "colour shirt" #by using format specifier 'r' you tell python to print it no matter what. 's' is used for printing string
