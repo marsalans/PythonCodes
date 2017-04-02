144b0b55ff41ae383b1b95eb3793d89071027073
FlashBlaze <neerajlagwankar@gmail.com>
2017-04-02 13:34:19 +0530

    Getting input and checking pydoc

--- examples2.py
+++ examples2.py
@@ -1,2 +1,25 @@
-print "What is your name?"
-age = raw_input()
+#taking inout from the user
+
+# print "What is your name?"
+# name = raw_input()
+# print "Your name is: %s" % name
+
+# print "What is your favourite programming language?"
+# lang = raw_input()
+# print "Oh it is %s" %lang
+
+# print "Enter a"          #observe this code, it is for taking input as integers
+# num1 = int(raw_input())
+# print "Enter b"
+# num2 = int(raw_input())
+# num3 = num1 + num2
+# print "Answer is %d" %num3
+
+ans = raw_input("Age?")
+print ans #prompting user directly in the raw input 
+
+# type python -m pydoc raw_input in cmd
+# skip python and -m for linux
+
+# Use pydoc to also read about open, file, os, and sys. 
+
